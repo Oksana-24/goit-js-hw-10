0d5b9b60774577b76c92c4103fbbc2ced06e601d
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-21 13:42:24 +0100

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,12 +1,12 @@
 
 function fetchCountries(name) {
-    return response= fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
+    return (response= fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`)
         .then(response => {
             if (!response.status) {
                 throw new Error(response.statusText);
             }
             return response.json();
-    })
+    }))
     
 }
 
