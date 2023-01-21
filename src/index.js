34cea5f1f882a0b6be92893cf9c6f6563f59bf28
Oksana Khymych <66249893+Oksana-24@users.noreply.github.com>
2023-01-21 10:52:14 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,61 +8,62 @@ const DEBOUNCE_DELAY = 300;
 
 const input = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
-const countryInfo = document.querySelector('.country-info');
+// const countryInfo = document.querySelector('.country-info');
 
 input.addEventListener('input', debounce(onSearchCountry, DEBOUNCE_DELAY));
 
 function onSearchCountry(event) {
-
-
     const nameCountry = input.value.trim();
 
+    if (!nameCountry) {
+        countryList.innerHTML = '';
+        return
+    }
 
         fetchCountries(nameCountry)
-        .then(data => {console.log(data)
-            // if (data.length > 10) {
-            //     Notify.warning('Too many matches found. Please enter a more specific name.');
-            //     return
-            // }
-            // createMarkup(data);
+        .then(data => {
+            if (data.length > 10) {
+                Notify.warning('Too many matches found. Please enter a more specific name.');
+                return
+            }
+            createMarkup(data);
         })
-        // .catch(error => {
-        //     if (error.name === 'Error') {
-        //         Notify.failure('Oops, there is no country with that name');
-        //         return
-        //     }
-        //     })
+        .catch(error => {
+            if (error.name === 'Error') {
+                Notify.failure('Oops, there is no country with that name');
+                return
+            }
+            })
     
 }
 
-// function createMarkup(data) {
-//     console.log("DATA",data)
-//     if (data.length === 1) {
-//         const markup = data.map(({
-//             name: { official: nameCountry },
-//             flags: { svg },
-//             capital,
-//             population,
-//             languages }) =>
-//             `<li>
-//         <h2>Country: ${nameCountry}</h2>
-//         <img src="${svg}" alt="${nameCountry} width ="300" />
-//         <h2>Capital: ${capital}</h2>
-//         <p>Population: ${population}</p>
-//         <p>Languages: ${languages}</p>
-//         </li>`
-//         ).join('');
+function createMarkup(data) {
+    console.log("DATA",data)
+    if (data.length === 1) {
+        const markup = data.map(({
+            name: { official: nameCountry },
+            flags: { svg },
+            capital,
+            population,
+            languages }) =>  `<li>
+        <h2>${nameCountry}</h2>
+        <img src="${svg}" alt="${nameCountry} width ="300" />
+        <h2>Capital: ${capital}</h2>
+        <p>Population: ${population}</p>
+        <p>Languages: ${languages}</p>
+        </li>`
+            ).join('');
 
 
-//         countryList.innerHTML = markup;
-//     }
-//     // if (data.length > 1 && data.length <= 10) {
-//     //     const markup = data.map(({
-//     //         name: { official: nameCountry },
-//     //         flags: { svg } }) =>
-//     //         `<h2>Country: ${nameCountry}</h2>
-//     //         <img src="${svg}" alt="${nameCountry}" width ="200">`).join('');
+        countryList.innerHTML = markup;
+    }
+    if (data.length > 1 && data.length <= 10) {
+        const markup = data.map(({
+            name: { official: nameCountry },
+            flags: { svg } }) =>
+            `<h2>Country: ${nameCountry}</h2>
+            <img src="${svg}" alt="${nameCountry}" width ="200">`).join('');
         
-//     //     countryInfo.innerHTML = markup;
-//     // }
-// }
+        countryList.innerHTML = markup;
+    }
+}
